96fdbd6ea4933fa407b01648af613e29f96c0aea
jyouj <youj.whenever.prime3@gmail.com>
2019-04-14 22:03:47 +0900

    LINE Bot のサンプル

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
@@ -15,11 +16,41 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
+	// create line bot
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	router := gin.New()
 	router.Use(gin.Logger())
 
+	// https://***.com/
 	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello, World!", nil)
+	})
+
+	router.POST("/callback", func(c *gin.Context) {
+		events, err := bot.ParseRequest(c.Request)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				log.Print(err)
+			}
+			return
+		}
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
 	})
 
 	router.Run(":" + port)
