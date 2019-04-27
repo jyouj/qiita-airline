fc1746d0c7d4467979864315ff2866e668ba682b
jyouj <youj.whenever.prime3@gmail.com>
2019-04-27 23:54:20 +0900

    using chromedriver

--- controller/controller.go
+++ controller/controller.go
@@ -2,10 +2,11 @@ package controller
 
 import (
   "log"
-  "net/http"
+  // "net/http"
   "strings"
 
   "github.com/PuerkitoBio/goquery"
+  "github.com/sclevine/agouti"
 )
 
 func QiitaController(message string) string {
@@ -19,9 +20,9 @@ func QiitaController(message string) string {
     result := QiitaSearch(url)
     return result
   case "-h":
-    result := `QiitaAirlineのご利用方法のご案内\n
-    -s: Qiitaの人気記事を3つ検索します\n
-    -t <検索ワード>: 検索ワードにヒットしたQiita記事を3つお届けします\n
+    result := `QiitaAirlineのご利用方法のご案内
+    -s: Qiitaの人気記事を3つ検索します
+    -t <検索ワード>: 検索ワードにヒットしたQiita記事を3つお届けします
     -h: 使い方ガイドを表示します`
     return result
   }
@@ -30,18 +31,31 @@ func QiitaController(message string) string {
 
 // scraiping for Qiita
 func QiitaSearch(url string) string {
-  // request url
-  res, err := http.Get(url)
-  if err != nil {
-    log.Fatal(err)
+  // Start ChromeDriver
+  driver := agouti.ChromeDriver(agouti.Browser("chrome"))
+  if err := driver.Start(); err != nil {
+      log.Fatalf("Failed to start driver:%v", err)
   }
-  defer res.Body.Close()
-  if res.StatusCode != 200 {
-    log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+  defer driver.Stop()
+
+  page, err := driver.NewPage()
+  if err != nil {
+      log.Fatalf("Failed to open page:%v", err)
   }
 
+  if err := page.Navigate(url); err != nil {
+      log.Fatalf("Failed to navigate:%v", err)
+  }
+
+  content, err := page.HTML()
+  if err != nil {
+    log.Printf("Failed to get html: %v", err)
+  }
+
+  res := strings.NewReader(content)
+
   // read HTML
-  doc, err := goquery.NewDocumentFromReader(res.Body)
+  doc, err := goquery.NewDocumentFromReader(res)
   if err != nil {
     log.Fatal(err)
   }
@@ -53,7 +67,7 @@ func QiitaSearch(url string) string {
   var boxes QiitaBoxes
 
   // scraipe
-  doc.Find("div.p-home_main").EachWithBreak(func(i int, s *goquery.Selection) bool {
+  doc.Find(".tr-item").EachWithBreak(func(i int, s *goquery.Selection) bool {
     // get title & author
     box.Title = s.Find("a.tr-Item_title").Text()
     box.Author = s.Find("a.tr-Item_author").Text()
